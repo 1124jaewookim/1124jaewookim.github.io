99d50a4dd4b2befa5caeb06fc1d23677ef8ec57d
1124jaewookim <48208784+1124jaewookim@users.noreply.github.com>
2023-03-01 01:44:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["AI Researcher", "MS/PhD Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["AI Researcher", "MS/PhD Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
